c2ead24f7223a1b51810d43c54bc2b2a28067418
Mritunjay Kumar <ashupost@gmail.com>
2015-09-09 22:00:41 +0530

    js

---
+++ ground-common/src/main/java/com/ground/nashorn/ScriptEngineExample.java
@@ -0,0 +1,26 @@
+package com.ground.nashorn;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+public class ScriptEngineExample {
+	public static void main(String args[]) {
+
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
+
+		String name = "Mahesh";
+		Integer result = null;
+
+		try {
+			nashorn.eval("print('" + name + "')");
+			result = (Integer) nashorn.eval("10 + 2");
+
+		} catch (ScriptException e) {
+			System.out.println("Error executing script: " + e.getMessage());
+		}
+
+		System.out.println(result.toString());
+	}
+}
